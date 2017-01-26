d0f0dae31442cc1aaf8b4469ee5d929dcf269025
M.W <mogeiwang@gmail.com>
2017-01-26 17:33:38 +0800

    Update extract_reposts.sh

--- extract_reposts.sh
+++ extract_reposts.sh
@@ -2,4 +2,5 @@
 #  -*- coding:utf-8 -*-
 
 # -F to define the separator '^A' used in the data file
-awk -F'\x01' '{print $2 " " $3 " " $4}' trainRepost.txt > reposts.txt
+awk -F'\x01' '{print $1 " " $2 " " $3 " " $4}' trainRepost.txt > reposts.txt
+# postID poster transposter transpost_moment_from_begining
